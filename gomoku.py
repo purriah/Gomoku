ed57ed6b23b5bdb99ff188847ae38a5d0f4e60c1
Ibrahim Altaweel <Abe@Abes-MacBook-Pro.local>
2014-05-26 17:53:36 -0700

    bug fixes, illegal moves/input handling.

--- gomoku.py
+++ gomoku.py
@@ -13,15 +13,27 @@ class Gomoku:
 		while True:
 			turn_x = raw_input("Player 1 turn x: ")	
 			turn_y = raw_input("Player 1 turn y: ")
+			
+			while not (self.player1.check_legal(self.board,int(turn_x),int(turn_y))):
+				turn_x = raw_input("Player 1 turn x: ")	
+				turn_y = raw_input("Player 1 turn y: ")
 			self.player1.move(self.board,int(turn_x),int(turn_y))
-			if player1.win(self.board,turn_x,turn_y):
-				print("Play 1 wins")
+			
+			if self.player1.win(self.board,int(turn_x),int(turn_y)):
+				print("Player 1 wins")
 				return
+			
 			turn_x = raw_input("Player 2 turn x: ")	
 			turn_y = raw_input("Player 2 turn y: ")
+
+
+			while not (self.player2.check_legal(self.board,int(turn_x),int(turn_y))):
+				turn_x = raw_input("Player 2 turn x: ")	
+				turn_y = raw_input("Player 2 turn y: ")
 			self.player2.move(self.board,int(turn_x),int(turn_y))
-			if player1.win(self.board,turn_x,turn_y):
-				print("Play 2 wins")
+			
+			if self.player2.win(self.board,int(turn_x),int(turn_y)):
+				print("Player 2 wins")
 				return
 
 		
--- player.py
+++ player.py
@@ -7,56 +7,99 @@ class Player:
 		board.config[x][y]=self.color
 		print(board.toString())
 
+	def check_legal(self,board,x,y):
+		
+		if x>15 or x<0:
+			
+			return False
+		if y>15 or y<0:
+			
+			return False
+		if board.config[x][y] !=0:
+			
+			return False
+		
+		return True
+
 	def win(self,board,pivot_x,pivot_y):
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x-1][pivot_y] == self.color) && (board[pivot_x-2][pivot_y] == self.color) && (board[pivot_x-3][pivot_y] == self.color) && (board[pivot_x-4][pivot_y] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x-1][pivot_y] == self.color) && (board[pivot_x-2][pivot_y] == self.color) && (board[pivot_x-3][pivot_y] == self.color) && (board[pivot_x+1][pivot_y] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x-1][pivot_y] == self.color) && (board[pivot_x-2][pivot_y] == self.color) && (board[pivot_x-3][pivot_y] == self.color) && (board[pivot_x-4][pivot_y] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x-1][pivot_y] == self.color) && (board[pivot_x-2][pivot_y] == self.color) && (board[pivot_x+1][pivot_y] == self.color) && (board[pivot_x+2][pivot_y] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x-1][pivot_y] == self.color) && (board[pivot_x+1][pivot_y] == self.color) && (board[pivot_x+2][pivot_y] == self.color) && (board[pivot_x+3][pivot_y] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x+1][pivot_y] == self.color) && (board[pivot_x+2][pivot_y] == self.color) && (board[pivot_x+3][pivot_y] == self.color) && (board[pivot_x+4][pivot_y] == self.color):
-			return True
 
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x][pivot_y-1] == self.color) && (board[pivot_x][pivot_y-2] == self.color) && (board[pivot_x][pivot_y-3] == self.color) && (board[pivot_x][pivot_y-4] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x][pivot_y-1] == self.color) && (board[pivot_x][pivot_y-2] == self.color) && (board[pivot_x][pivot_y-3] == self.color) && (board[pivot_x][pivot_y+1] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x][pivot_y-1] == self.color) && (board[pivot_x][pivot_y-2] == self.color) && (board[pivot_x][pivot_y-3] == self.color) && (board[pivot_x][pivot_y-4] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x][pivot_y-1] == self.color) && (board[pivot_x][pivot_y-2] == self.color) && (board[pivot_x][pivot_y+1] == self.color) && (board[pivot_x][pivot_y+2] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x][pivot_y-1] == self.color) && (board[pivot_x][pivot_y+1] == self.color) && (board[pivot_x][pivot_y+2] == self.color) && (board[pivot_x][pivot_y+3] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x][pivot_y+1] == self.color) && (board[pivot_x][pivot_y+2] == self.color) && (board[pivot_x][pivot_y+3] == self.color) && (board[pivot_x][pivot_y+4] == self.color):
-			return True
+		return self.check_horiz(board,pivot_x,pivot_y) or self.check_vert(board,pivot_x,pivot_y) or self.check_diag1(board,pivot_x,pivot_y) or self.check_diag2(board,pivot_x,pivot_y)
 
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x-1][pivot_y-1] == self.color) && (board[pivot_x-2][pivot_y-2] == self.color) && (board[pivot_x-3][pivot_y-3] == self.color) && (board[pivot_x-4][pivot_y-4] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x-1][pivot_y-1] == self.color) && (board[pivot_x-2][pivot_y-2] == self.color) && (board[pivot_x-3][pivot_y-3] == self.color) && (board[pivot_x+1][pivot_y+1] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x-1][pivot_y-1] == self.color) && (board[pivot_x-2][pivot_y-2] == self.color) && (board[pivot_x-3][pivot_y-3] == self.color) && (board[pivot_x-4][pivot_y-4] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x-1][pivot_y-1] == self.color) && (board[pivot_x-2][pivot_y-2] == self.color) && (board[pivot_x+1][pivot_y+1] == self.color) && (board[pivot_x+2][pivot_y+2] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x-1][pivot_y-1] == self.color) && (board[pivot_x+1][pivot_y+1] == self.color) && (board[pivot_x+2][pivot_y+2] == self.color) && (board[pivot_x+3][pivot_y+3] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x+1][pivot_y+1] == self.color) && (board[pivot_x+2][pivot_y+2] == self.color) && (board[pivot_x+3][pivot_y+3] == self.color) && (board[pivot_x+4][pivot_y+4] == self.color):
-			return True
 
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x-1][pivot_y+1] == self.color) && (board[pivot_x-2][pivot_y+2] == self.color) && (board[pivot_x-3][pivot_y+3] == self.color) && (board[pivot_x-4][pivot_y+4] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x-1][pivot_y+1] == self.color) && (board[pivot_x-2][pivot_y+2] == self.color) && (board[pivot_x-3][pivot_y+3] == self.color) && (board[pivot_x+1][pivot_y-1] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x-1][pivot_y+1] == self.color) && (board[pivot_x-2][pivot_y+2] == self.color) && (board[pivot_x-3][pivot_y+3] == self.color) && (board[pivot_x-4][pivot_y+4] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x-1][pivot_y+1] == self.color) && (board[pivot_x-2][pivot_y+2] == self.color) && (board[pivot_x+1][pivot_y-1] == self.color) && (board[pivot_x+2][pivot_y-2] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x-1][pivot_y+1] == self.color) && (board[pivot_x+1][pivot_y-1] == self.color) && (board[pivot_x+2][pivot_y-2] == self.color) && (board[pivot_x+3][pivot_y-3] == self.color):
-			return True
-		if (board[pivot_x][pivot_y] == self.color) && (board[pivot_x+1][pivot_y-1] == self.color) && (board[pivot_x+2][pivot_y-2] == self.color) && (board[pivot_x+3][pivot_y-3] == self.color) && (board[pivot_x+4][pivot_y-4] == self.color):
-			return True
+	def check_horiz(self,board,pivot_x,pivot_y):
+		if (pivot_x-4)>=0:
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x-1][pivot_y] == self.color) and (board.config[pivot_x-2][pivot_y] == self.color) and (board.config[pivot_x-3][pivot_y] == self.color) and (board.config[pivot_x-4][pivot_y] == self.color):
+				return True
+		if (pivot_x-3>=0 and pivot_x+1<15):
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x-1][pivot_y] == self.color) and (board.config[pivot_x-2][pivot_y] == self.color) and (board.config[pivot_x-3][pivot_y] == self.color) and (board.config[pivot_x+1][pivot_y] == self.color):
+				return True
+		if (pivot_x-2>=0 and pivot_x+2<15):
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x-1][pivot_y] == self.color) and (board.config[pivot_x-2][pivot_y] == self.color) and (board.config[pivot_x+1][pivot_y] == self.color) and (board.config[pivot_x+2][pivot_y] == self.color):
+				return True
+		if (pivot_x-1>=0 and pivot_x+3<15):
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x-1][pivot_y] == self.color) and (board.config[pivot_x+1][pivot_y] == self.color) and (board.config[pivot_x+2][pivot_y] == self.color) and (board.config[pivot_x+3][pivot_y] == self.color):
+				return True
+		if (pivot_x+4<15):
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x+1][pivot_y] == self.color) and (board.config[pivot_x+2][pivot_y] == self.color) and (board.config[pivot_x+3][pivot_y] == self.color) and (board.config[pivot_x+4][pivot_y] == self.color):
+				return True
+	
+	def check_vert(self,board,pivot_x,pivot_y):
+		if (pivot_y-4)>=0:
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x][pivot_y-1] == self.color) and (board.config[pivot_x][pivot_y-2] == self.color) and (board.config[pivot_x][pivot_y-3] == self.color) and (board.config[pivot_x][pivot_y-4] == self.color):
+				return True
+		if (pivot_y-3>=0 and pivot_y+1<15):
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x][pivot_y-1] == self.color) and (board.config[pivot_x][pivot_y-2] == self.color) and (board.config[pivot_x][pivot_y-3] == self.color) and (board.config[pivot_x][pivot_y+1] == self.color):
+				return True
+		if (pivot_x-2>=0 and pivot_y+2<15):
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x][pivot_y-1] == self.color) and (board.config[pivot_x][pivot_y-2] == self.color) and (board.config[pivot_x][pivot_y+1] == self.color) and (board.config[pivot_x][pivot_y+2] == self.color):
+				return True
+		if (pivot_y-1>=0 and pivot_y+3<15):
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x][pivot_y-1] == self.color) and (board.config[pivot_x][pivot_y+1] == self.color) and (board.config[pivot_x][pivot_y+2] == self.color) and (board.config[pivot_x][pivot_y+3] == self.color):
+				return True
+		if (pivot_y+4<15):
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x][pivot_y+1] == self.color) and (board.config[pivot_x][pivot_y+2] == self.color) and (board.config[pivot_x][pivot_y+3] == self.color) and (board.config[pivot_x][pivot_y+4] == self.color):
+				return True
+	
+	def check_diag1(self,board,pivot_x,pivot_y):
+		if (pivot_x-4>=0 and pivot_y-4>=0):
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x-1][pivot_y-1] == self.color) and (board.config[pivot_x-2][pivot_y-2] == self.color) and (board.config[pivot_x-3][pivot_y-3] == self.color) and (board.config[pivot_x-4][pivot_y-4] == self.color):
+				return True
+		if (pivot_x-3>=0 and pivot_y-3>=0 and pivot_x+1 <15 and pivot_y+1 <15):
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x-1][pivot_y-1] == self.color) and (board.config[pivot_x-2][pivot_y-2] == self.color) and (board.config[pivot_x-3][pivot_y-3] == self.color) and (board.config[pivot_x+1][pivot_y+1] == self.color):
+				return True
+		if (pivot_x-2>=0 and pivot_y-2>=0 and pivot_x+2<15 and pivot_y+2 <15):
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x-1][pivot_y-1] == self.color) and (board.config[pivot_x-2][pivot_y-2] == self.color) and (board.config[pivot_x+1][pivot_y+1] == self.color) and (board.config[pivot_x+2][pivot_y+2] == self.color):
+				return True
+
+		if (pivot_x-1>=0 and pivot_y-1>=0 and pivot_x+3 <15 and pivot_y+3 <15):
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x-1][pivot_y-1] == self.color) and (board.config[pivot_x+1][pivot_y+1] == self.color) and (board.config[pivot_x+2][pivot_y+2] == self.color) and (board.config[pivot_x+3][pivot_y+3] == self.color):
+				return True
+		if (pivot_x+4<15 and pivot_y+4<15):
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x+1][pivot_y+1] == self.color) and (board.config[pivot_x+2][pivot_y+2] == self.color) and (board.config[pivot_x+3][pivot_y+3] == self.color) and (board.config[pivot_x+4][pivot_y+4] == self.color):
+				return True
+	
+	def check_diag2(self,board,pivot_x,pivot_y):
+		if (pivot_x-4>=0 and pivot_y+4<15):
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x-1][pivot_y+1] == self.color) and (board.config[pivot_x-2][pivot_y+2] == self.color) and (board.config[pivot_x-3][pivot_y+3] == self.color) and (board.config[pivot_x-4][pivot_y+4] == self.color):
+				return True
+
+		if (pivot_x-3>=0 and pivot_y+3<15 and pivot_x+1 <15 and pivot_y-1 >=0):
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x-1][pivot_y+1] == self.color) and (board.config[pivot_x-2][pivot_y+2] == self.color) and (board.config[pivot_x-3][pivot_y+3] == self.color) and (board.config[pivot_x+1][pivot_y-1] == self.color):
+				return True
+
+		if (pivot_x-2>=0 and pivot_y+2<15 and pivot_x+2<15 and pivot_y-2 >=0):
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x-1][pivot_y+1] == self.color) and (board.config[pivot_x-2][pivot_y+2] == self.color) and (board.config[pivot_x+1][pivot_y-1] == self.color) and (board.config[pivot_x+2][pivot_y-2] == self.color):
+				return True
+
+		if (pivot_x-1>=0 and pivot_y+1<15 and pivot_x+3 <15 and pivot_y-3 >=0):
+
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x-1][pivot_y+1] == self.color) and (board.config[pivot_x+1][pivot_y-1] == self.color) and (board.config[pivot_x+2][pivot_y-2] == self.color) and (board.config[pivot_x+3][pivot_y-3] == self.color):
+				return True
+
+		if (pivot_x+4<15 and pivot_y-4>=0):
+			
+			if (board.config[pivot_x][pivot_y] == self.color) and (board.config[pivot_x+1][pivot_y-1] == self.color) and (board.config[pivot_x+2][pivot_y-2] == self.color) and (board.config[pivot_x+3][pivot_y-3] == self.color) and (board.config[pivot_x+4][pivot_y-4] == self.color):
+				return True
+
+
 
